48bdf2b1ddc1cdde89154651c66610b00e3f7fd6
Neelakandan <neelakandan@multicorewareinc.com>
2018-11-15 16:19:52 +0530

    Inlining and ensuring a prior declaration

--- modules/core/include/opencv2/core/cuda/common.hpp
+++ modules/core/include/opencv2/core/cuda/common.hpp
@@ -91,7 +91,8 @@ namespace cv { namespace cuda
 {
     namespace device
     {
-        __host__ __device__ __forceinline__ int divUp(int total, int grain)
+        inline __host__ __device__ __forceinline__ int divUp(int total, int grain);
+        inline __host__ __device__ __forceinline__ int divUp(int total, int grain)
         {
             return (total + grain - 1) / grain;
         }
