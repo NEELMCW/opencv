c6a3b78aa8c366442db0a1f048f26de277583ad2
Neelakandan <neelakandan@multicorewareinc.com>
2018-11-27 10:12:58 +0530

    Remove duplicate inline access specifiers

--- modules/core/include/opencv2/core/cuda/common.hpp
+++ modules/core/include/opencv2/core/cuda/common.hpp
@@ -98,8 +98,8 @@ namespace cv { namespace cuda
 {
     namespace device
     {
-        inline __host__ __device__ __forceinline__ int divUp(int total, int grain);
-        inline __host__ __device__ __forceinline__ int divUp(int total, int grain)
+        __host__ __device__ __forceinline__ int divUp(int total, int grain);
+        __host__ __device__ __forceinline__ int divUp(int total, int grain)
         {
             return (total + grain - 1) / grain;
         }
